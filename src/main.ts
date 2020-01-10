00326b758244bdb12de7bac4c9c17003225c68fd
power_840 <liuyu19921017@gmail.com>
2020-01-10 17:45:08 +0800

    feat: curd

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import * as mongoose from 'mongoose';
+
+async function bootstrap() {
+  mongoose.connect('mongodb://localhost/nest-blog-api', {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+  });
+  const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(5000);
+}
+bootstrap();
---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Query,
+  Param,
+  Put,
+  Delete
+} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiProperty } from '@nestjs/swagger';
+import { PostModel } from './post.model';
+
+class CreatePostDto {
+  @ApiProperty({ description: '帖子标题', example: '帖子标题1' })
+  title: string;
+  @ApiProperty({ description: '帖子内容', example: '帖子内容1' })
+  content: string;
+}
+
+// tslint:disable-next-line: max-classes-per-file
+@Controller('posts')
+@ApiTags('posts')
+export class PostsController {
+  @Get()
+  @ApiOperation({ summary: '显示博客列表', description: '显示博客列表' })
+  async index() {
+    return await PostModel.find();
+  }
+
+  @Post()
+  @ApiOperation({ summary: '创建帖子' })
+  async create(
+    @Body() createPostDto: CreatePostDto,
+    @Query() query,
+    @Param() params
+  ) {
+    await PostModel.create(createPostDto);
+    return { success: true };
+  }
+
+  @Post(':id')
+  @ApiOperation({ summary: '显示详情' })
+  async detail(@Param('id') id: string) {
+    return await PostModel.findById(id);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: '编辑帖子' })
+  async update(@Param('id') id: string, @Body() updatePostDto: CreatePostDto) {
+    await PostModel.findByIdAndUpdate(id, updatePostDto);
+    return { success: true };
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除帖子' })
+  async remove(@Param('id') id: string) {
+    await PostModel.findByIdAndDelete(id);
+    return { success: true };
+  }
+}
